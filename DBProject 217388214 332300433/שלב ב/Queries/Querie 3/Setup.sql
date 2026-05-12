015786c98f42386025a641efff7c96cdcf769473
haggai <haggaias14@gmail.com>
2026-05-12 23:33:24 +0300

    added the test for querie 3

---
+++ DBProject 217388214 332300433/שלב ב/Queries/Querie 3/Setup.sql
@@ -0,0 +1,4 @@
+INSERT INTO DRONES (Drone_ID, Serial_Number, Purchase_Date, Model_ID) VALUES (700, 'TEST_DRONE', '2000-03-15', 1);
+
+INSERT INTO MAINTENANCE_LOGS (Log_ID, Maintenance_Date, Work_Description, Cost, Drone_ID) VALUES
+  (30000, '2026-03-15', 'testing querie 3', 0.00, 700);
---
+++ DBProject 217388214 332300433/שלב ב/Queries/Querie 3/Teardown.sql
@@ -0,0 +1,5 @@
+delete from maintenance_logs
+where log_id = 30000;
+
+DELETE from drones
+where drone_id = 700;
