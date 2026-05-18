93bb189e93b85d15dd20af105f4a883483bfcdfb
haggai <haggaias14@gmail.com>
2026-05-18 23:16:09 +0300

    worte the 5th Querie

---
+++ DBProject 217388214 332300433/שלב ב/Queries/Querie 5.sql
@@ -0,0 +1,6 @@
+select o.first_name, o.last_name, STRING_AGG(dm.model_name, ' , ') as expiring_models
+from operator_certifications oc
+JOIN drone_models dm ON oc.model_id = dm.model_id
+JOIN operators o ON oc.operator_id = o.operator_id
+WHERE oc.license_expiration_date BETWEEN current_date and current_date + interval '60 days'
+group by oc.operator_id, o.first_name, o.last_name;
