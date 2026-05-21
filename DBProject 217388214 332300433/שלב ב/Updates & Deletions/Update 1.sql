b7effcb8703c8c967753d2fef90ac06ad589b2b4
haggai <haggaias14@gmail.com>
2026-05-21 18:19:04 +0300

    added screan shouts of the execution of the deletion & update queries

--- DBProject 217388214 332300433/שלב ב/Updates & Deletions/Update 1.sql
+++ DBProject 217388214 332300433/שלב ב/Updates & Deletions/Update 1.sql
@@ -5,7 +5,7 @@ SELECT * FROM missions;
 
 update missions
 set mission_status = 'Review Required'
-where m.mission_id IN (select ma.mission_id
+where mission_id IN (select ma.mission_id
                         from mission_assignments ma
                         JOIN flight_alerts fa ON ma.assignment_id = fa.assignment_id
                         where fa.severity_level = 5);
