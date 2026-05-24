b16886c31cbc50a2bb5b0d7ea714611c166934ff
haggai <haggaias14@gmail.com>
2026-05-25 00:47:10 +0300

    changed the EXPLAIN analyze.

--- DBProject 217388214 332300433/שלב ב/Indexs/Index 2/time improvement.sql
+++ DBProject 217388214 332300433/שלב ב/Indexs/Index 2/time improvement.sql
@@ -1,2 +1,2 @@
 EXPLAIN analyze
-select * from maintenance_logs where maintenance_date >= '2025-01-01';
+select * from maintenance_logs where maintenance_date = '2026-04-07';
--- DBProject 217388214 332300433/שלב ב/Indexs/Index 3/time improvement.sql
+++ DBProject 217388214 332300433/שלב ב/Indexs/Index 3/time improvement.sql
@@ -1,2 +1,4 @@
 EXPLAIN analyze
-select * from flight_alerts where alert_timestamp >= '2025-01-01 00:00:00';
+select * from flight_alerts 
+where alert_timestamp >= '2026-03-26 00:00:00'
+AND   alert_timestamp < '2026-03-27 00:00:00';
